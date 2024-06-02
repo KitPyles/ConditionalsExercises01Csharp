30c8f32ecfcfd55b7b8625413e4441901e8e3548
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2024-06-02 02:06:37 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        // Declare and initialize the variables for exercise 1 here:
+        string engineIndicatorLight = "red blinking";
+        bool spaceSuitsOn = true;
+        bool shuttleCabinReady = true;
+        bool crewStatus = spaceSuitsOn && shuttleCabinReady;
+        int computerStatus
+  
+
+
+    // BEFORE running the code, predict what will be printed to the console by the following statements: "engines are off"
+
+        {
+        {
+          } 
+        }
+        else if (engineIndicatorLight == "green blinking")
+        {
+            Console.WriteLine("engines are preparing to start");
+        }
+        else
+        {
+            Console.WriteLine("engines are off");
+        }
+  }
